baee0d2f4c33682d2a22cd6ce08a62577ac6f743
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-20 15:53:10 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,13 +16,15 @@ function init() {
   const input = document.querySelector('input');
   const log = document.getElementById('log');
   
+      input.addEventListener('keydown', logKey);
+
+   function logKey(e) {
+     log.textContent += ` ${e.code}`;
+    }
+  
+  
   document.body.addEventListener("keydown",(event));{
      
-
-
-
-  
-  
   }
   
   
