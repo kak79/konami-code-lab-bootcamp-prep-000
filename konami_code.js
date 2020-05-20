bb4ee32df4afbd903caffb758262c9646e354d65
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-20 17:29:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -26,10 +26,10 @@ let index = 0;
        index = 0;
      }
    } 
-     else {
-     index = 9;
-     alert("Hurray!");
-   }
+    // else {
+     //index = 9;
+     //alert("Hurray!");
+  // }
  
      
   });
